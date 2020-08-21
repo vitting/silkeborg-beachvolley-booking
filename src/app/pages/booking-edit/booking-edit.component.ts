d7392443efca886b3e4f9b095631b6151cf6d1e1
Christian Nicolaisen <cvn_vitting@hotmail.com>
2020-08-21 09:27:16 +0200

    update

--- src/app/pages/booking-edit/booking-edit.component.ts
+++ src/app/pages/booking-edit/booking-edit.component.ts
@@ -1,11 +1,64 @@
 import { Component, OnInit } from "@angular/core";
+import {
+  MatChipListChange,
+  MatChipSelectionChange,
+} from "@angular/material/chips";
+
+interface BookingTime {
+  text: string;
+  selected: boolean;
+}
 
 @Component({
   templateUrl: "./booking-edit.component.html",
   styleUrls: ["./booking-edit.component.scss"],
 })
 export class BookingEditComponent implements OnInit {
+  bookingTimes: BookingTime[] = [
+    {
+      selected: false,
+      text: "Kl. 08.00-10.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 10.00-12.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 12.00-14.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 14.00-16.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 16.00-18.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 18.00-20.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 20.00-22.00",
+    },
+    {
+      selected: false,
+      text: "Kl. 22.00-24.00",
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
+
+  bookingTimeChange(item: BookingTime): void {
+    for (const time of this.bookingTimes) {
+      if (item !== time) {
+        time.selected = false;
+      }
+    }
+
+    item.selected = !item.selected;
+  }
 }
--- src/app/pages/booking-edit/booking-edit.module.ts
+++ src/app/pages/booking-edit/booking-edit.module.ts
@@ -3,9 +3,28 @@ import { CommonModule } from "@angular/common";
 
 import { BookingEditRoutingModule } from "./booking-edit-routing.module";
 import { BookingEditComponent } from "./booking-edit.component";
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [BookingEditComponent],
-  imports: [CommonModule, BookingEditRoutingModule],
+  imports: [
+    CommonModule,
+    BookingEditRoutingModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
 })
 export class BookingEditModule {}
